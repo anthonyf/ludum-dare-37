33190ddb1a45ceb113241e2ae44d7a903facf02b
Anthony Fairchild <fairchild.anthony@gmail.com>
2016-12-10 16:12:29 -0500

    fixed images

---
+++ src/ld37/asset_manager.clj
@@ -0,0 +1,22 @@
+(ns ld37.asset-manager
+  (:import [com.badlogic.gdx.graphics Texture]
+           [com.badlogic.gdx.graphics.g2d GlyphLayout]
+           [com.badlogic.gdx.assets AssetManager]
+           [com.badlogic.gdx.assets.loaders.resolvers InternalFileHandleResolver]
+           [com.badlogic.gdx.graphics.g2d.freetype
+            FreeTypeFontGenerator FreeTypeFontGeneratorLoader FreetypeFontLoader
+            FreetypeFontLoader$FreeTypeFontLoaderParameter]
+           (com.badlogic.gdx.graphics.g2d BitmapFont TextureRegion NinePatch)
+           (com.badlogic.gdx.scenes.scene2d.ui Image ImageTextButton
+                                               ImageTextButton$ImageTextButtonStyle)
+           (com.badlogic.gdx.utils Scaling Align)
+           (com.badlogic.gdx.scenes.scene2d.utils TextureRegionDrawable NinePatchDrawable)))
+
+(defn setup-asset-manager []
+  (let [manager (AssetManager.)
+        resolver (InternalFileHandleResolver.)]
+    (.setLoader manager FreeTypeFontGenerator (FreeTypeFontGeneratorLoader. resolver))
+    (.setLoader manager BitmapFont ".ttf" (FreetypeFontLoader. resolver))
+    manager))
+
+(def manager (setup-asset-manager))
