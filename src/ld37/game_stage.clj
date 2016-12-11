7320dc3dc998fad6de3e97b6f1c15da43b91856e
Anthony Fairchild <fairchild.anthony@gmail.com>
2016-12-10 20:31:16 -0500

    improved move keys

--- src/ld37/game_stage.clj
+++ src/ld37/game_stage.clj
@@ -19,31 +19,39 @@
     :up
 
     (.isKeyPressed Gdx/input Input$Keys/DOWN)
-    :down))
+    :down
+
+    :else nil))
 
 (defn game-tick!
-  [stage game]
-  (reset! game (if-let [direction (input-direction stage game)]
-                 (snake/move @game direction)
-                 (snake/move-forward @game))))
+  [stage data game]
+  (let [{:keys [next-move]} @data]
+    (reset! game (if next-move
+                   (do (reset! data (assoc @data :next-move nil))
+                       (snake/move @game next-move))
+                   (snake/move-forward @game)))))
 
-(defn update-game! [stage game]
-  (let [{:keys [::time-since-update]} @game
+(defn update-game! [stage data game]
+  (let [{:keys [::time-since-update]} @data
         delta-time (.getDeltaTime Gdx/graphics)
         elapsed-time (+ delta-time (or time-since-update 0.0))]
-    (if (> elapsed-time
-           c/game-speed)
-      (do (reset! game (assoc @game ::time-since-update 0))
-          (game-tick! stage game))
-      (reset! game (assoc @game ::time-since-update elapsed-time)))))
+
+    (if-let [direction (input-direction stage game)]
+      (reset! data (assoc @data :next-move direction)))
+
+    (if (> elapsed-time c/game-speed)
+      (do (reset! data (assoc @data ::time-since-update 0))
+          (game-tick! stage data game))
+      (reset! data (assoc @data ::time-since-update elapsed-time)))))
 
 (defn make-game-stage
   [game]
-  (let [[screen-width screen-height] c/screen-size
+  (let [data (atom {})
+        [screen-width screen-height] c/screen-size
         stage (proxy [Stage]
                   [(FitViewport. screen-width screen-height)]
                   (act []
-                    (update-game! this game)
+                    (update-game! this data game)
                     (proxy-super act))
                   (draw []
                     (proxy-super draw)))
